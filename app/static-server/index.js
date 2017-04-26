0c56d9d0e71e1c5ac6395e2bf901ff3b64774d41
PeterCheng1 <1284995325@qq.com>
2017-04-26 16:48:50 +0800

    asyncReadFile

--- app/static-server/index.js
+++ app/static-server/index.js
@@ -2,8 +2,6 @@
 *Koa2重构pure-node-note author:Sheldon-Yee  
 *static-server
 */
-
-
 const path = require('path');
 const fs = require('fs');
 const mime = require('mime');
@@ -11,8 +9,21 @@ const getPath = (url) => {
     return path.resolve(process.cwd(), 'public', `./${url}`)
 }
 
+const readFile  = (_path) =>{
+    return new Promise((resolve,reject)=>{
+        fs.readFile(_path,(error,data)=>{
+            if(error) {
+                reject(error);
+            }else{  
+                resolve(data);
+            }
+ 
+        })
+    })
+}
+
 const StaticFun = () => {
-    return async(ctx, next) => {
+    return async (ctx, next) => {
         let {url} = ctx;
         if (url.match('action')) {
             next()
@@ -23,9 +34,15 @@ const StaticFun = () => {
             let _path = getPath(url);
             let _type = mime.lookup(_path);
             ctx.type = _type;
+            // let body = fs.readFileSync(_path);
+            // debugger;
             ctx.body = fs.readFileSync(_path);
+            let body = await readFile(_path)
+            return ctx.body = body ;
+            // debugger;
         }
     }
 }
-
 module.exports = StaticFun
+
+
--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-// 导入koa，和koa 1.x不同，在koa2中，我们导入的是一个class，因此用大写的Koa表示:
+
 const Koa = require('koa');
 const path = require('path');
 const staticFun = require('./app/static-server/index.js')//静态服务器中间件
 const apiServer = require('./app/api-server/index.js')//Api服务器中间件
-// 创建一个Koa对象表示web app本身:
+
 const app = new Koa();
 
+
+//中间件
 app.use(apiServer());
 app.use(staticFun());
+
+
 
 app.listen(3000);
 console.log('app started at port 3030...');
